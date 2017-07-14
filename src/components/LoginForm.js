a993c127019d0341efd0c06bc98cfad267ae3958
Jenny Steele <jenny@jennyscript.com>
2017-07-14 17:10:08 -0400

    Show loading spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,28 +2,36 @@ import React, { Component } from 'react'
 import { Text } from 'react-native'
 import firebase from 'firebase'
 
-import { Button, Card, CardSection, Input } from './common'
+import { Button, Card, CardSection, Input, Spinner } from './common'
 
 export default class LoginForm extends Component {
   constructor(props) {
     super(props)
     this.onButtonPress = this.onButtonPress.bind(this)
+    this.onLoginSuccess = this.onLoginSuccess.bind(this)
+    this.onLoginFail = this.onLoginFail.bind(this)
   }
-  state = { email: '', password: '', error: '' }
+  state = { email: '', password: '', error: '', loading: false }
   onButtonPress() {
     const { email, password } = this.state
-    this.setState({ error: '' })
+    this.setState({ error: '', loading: true })
     firebase
       .auth()
       .signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess)
       .catch(() => {
         firebase
           .auth()
           .createUserWithEmailAndPassword(email, password)
-            .catch(() => {
-              this.setState({ error: 'Could not log in or create an account' })
-            })
+          .then(this.onLoginSuccess)
+          .catch(this.onLoginFail)
       })
+  }
+  onLoginSuccess() {
+    this.setState({ email: '', password: '', error: '', loading: false })
+  }
+  onLoginFail() {
+    this.setState({ error: 'Could not log in or create an account', loading: false })
   }
   render() {
     return (
@@ -49,9 +57,12 @@ export default class LoginForm extends Component {
           {this.state.error}
         </Text> : null}
         <CardSection>
-          <Button onPress={this.onButtonPress}>
-            Log in
-          </Button>
+          {this.state.loading ?
+            <Spinner size="small" /> :
+            <Button onPress={this.onButtonPress}>
+              Log in
+            </Button>
+          }
         </CardSection>
       </Card>
     )
--- src/components/common/Spinner.js
+++ src/components/common/Spinner.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, ActivityIndicator } from 'react-native'
 
-export const Spinner = () =>
-  <View style={styles.spinner} />
+export const Spinner = ({ size }) =>
+  <View style={styles.spinner}>
+    <ActivityIndicator size={size || 'large'} />
+  </View>
 
 
 const styles = {
-  spinner: {}
+  spinner: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
 }
