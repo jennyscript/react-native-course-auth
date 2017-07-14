7a34ed48ebc45b28268aec556b1dc0537386620f
Jenny Steele <jenny@jennyscript.com>
2017-07-14 17:41:51 -0400

    Implement log out

--- src/App.js
+++ src/App.js
@@ -2,20 +2,45 @@ import React, { Component } from 'react'
 import { View } from 'react-native'
 import firebase from 'firebase'
 
-import { Header } from './components/common'
+import { Button, Header, Spinner } from './components/common'
 import LoginForm from './components/LoginForm'
 import firebaseConfig from '../firebase-config'
 
 export default class App extends Component {
+  state = { loggedIn: null }
   componentWillMount() {
     firebase.initializeApp(firebaseConfig)
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ loggedIn: !!user })
+    })
+  }
+  renderContent() {
+    if (this.state.loggedIn === false) {
+      return <LoginForm />
+    }
+    return (
+      <View style={styles.container}>
+        {this.state.loggedIn ?
+          <Button onPress={() => firebase.auth().signOut()}>Log out</Button>
+          : null
+        }
+        {this.state.loggedIn === null ? <Spinner /> : null}
+      </View>
+    )
   }
   render() {
     return (
       <View>
         <Header>Authentication</Header>
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
+  }
+}
+
+const styles = {
+  container: {
+    paddingTop: 10,
+    flexDirection: 'row'
   }
 }
